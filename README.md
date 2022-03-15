15a28e3de2fda7b70203afa962f616933286284e
kbwoodall <kerry.woodall@gmail.com>
2022-03-14 18:04:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-query-on-success-issue-m9tb2d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-query-on-success-issue-m9tb2d)
---
+++ index.js
@@ -0,0 +1,37 @@
+import React, { useEffect, Fragment } from 'react';
+import { render } from 'react-dom';
+import { useQuery } from 'react-query';
+
+import Hello from './Hello';
+import './style.css';
+//https://kwoodallsapp.herokuapp.com/
+//https://kwoodallsapp.herokuapp.com/getitems
+//fetch("https://jsonplaceholder.typicode.com/posts?_limit=2")
+
+const App = () => {
+  const { data = [], status } = useQuery({
+    queryKey: 'todos',
+    queryFn: () =>
+      fetch('https://kwoodallsapp.herokuapp/getitems').then((res) =>
+        res.json()
+      ),
+    config: {
+      onSuccess: (data) => [...data].reverse(),
+    },
+  });
+
+  return (
+    <div>
+      <Hello name={'world'} />
+      <p>Start editing now to see some magic happen :)</p>
+      {data.map(({ name, phone }, index) => (
+        <div key={index}>
+          <h3>{phone}</h3>
+          <p> {name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
